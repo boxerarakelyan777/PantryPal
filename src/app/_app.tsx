db09ab3ec576e17f51ca9ce9c0585e691045e397
Rudik Arakelyan <rudik.arakelyan777@gmail.com>
2024-07-29 19:35:42 -0700

    bug fix

--- src/app/_app.tsx
+++ src/app/_app.tsx
@@ -1,16 +1,17 @@
 // src/app/_app.tsx
 import React from 'react';
+import { AppProps } from 'next/app';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import theme from '../theme';
 
-function MyApp({ Component, pageProps }) {
-  return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Component {...pageProps} />
-    </ThemeProvider>
-  );
-}
-
-export default MyApp;
+function MyApp({ Component, pageProps }: AppProps) {
+    return (
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    );
+  }
+  
+  export default MyApp;
