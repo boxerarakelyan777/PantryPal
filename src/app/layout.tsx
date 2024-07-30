e91338ef7d89de5007322fa407a72d212515e73a
Rudik Arakelyan <rudik.arakelyan777@gmail.com>
2024-07-30 10:20:33 -0700

    hotjar and google analyitics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,21 +29,46 @@ export default function RootLayout({
         <link rel="icon" href="/FaviconIcon2.png" />
         
 
-              
         <Script 
           async 
-          src="https://www.googletagmanager.com/gtag/js?id=G-N8RLXQG6GK"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID}`}
         ></Script>
-        <Script id="google-analytics">
-          {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
 
-              gtag('config', 'G-N8RLXQG6GK');
 
-        `}
-        </Script>
+        <Script
+          id="HotjarAnalyitics"
+          dangerouslySetInnerHTML={{
+            __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', '${process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID}');
+            
+            
+            `,
+
+          }}
+        />
+
+        <Script
+          id="HotjarAnalyitics"
+          dangerouslySetInnerHTML={{
+            __html: `
+                (function(h,o,t,j,a,r){
+                  h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                  h._hjSettings={hjid:2327305,hjsv:6};
+                  a=o.getElementsByTagName('head')[0];
+                  r=o.createElement('script');r.async=1;
+                  r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                  a.appendChild(r);
+                })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+            
+            
+            `,
+
+          }}
+        />
 
       </head>
       <body className={inter.className} style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
