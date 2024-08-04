0c55462c12a8390745b49891ec780f68203f43b9
Rudik Arakelyan <rudik.arakelyan777@gmail.com>
2024-08-04 02:39:54 -0700

    Bug Fixes

--- src/pages/api/recipeGenerator.ts
+++ src/pages/api/recipeGenerator.ts
@@ -2,7 +2,7 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { Groq } from 'groq-sdk';
 
 const groq = new Groq({
-  apiKey: process.env.GROQ_API_KEY,
+  apiKey: process.env.NEXT_PUBLIC_GROQ_API_KEY,
 });
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
